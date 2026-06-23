4091fe194c342635c72fd6b4044e89766b6117b4
eduardo-toste <duciochetti@gmail.com>
2026-06-23 06:24:14 -0300

    initial project version upload

---
+++ src/main/java/com/auth/lab/AuthlabApplication.java
@@ -0,0 +1,13 @@
+package com.auth.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthlabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auth/lab/AuthlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
