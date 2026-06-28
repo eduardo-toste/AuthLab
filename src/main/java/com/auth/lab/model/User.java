40bf2f7f09f34634e52355dff453254f9a53c4c6
eduardo-toste <duciochetti@gmail.com>
2026-06-28 20:55:22 -0300

    fix: update role foreign key field name

--- src/main/java/com/auth/lab/model/User.java
+++ src/main/java/com/auth/lab/model/User.java
@@ -34,7 +34,7 @@ public class User implements UserDetails {
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role-id"))
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
     @Override
