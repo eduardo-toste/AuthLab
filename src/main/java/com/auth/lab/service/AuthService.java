6434859262d5075056d9d8ab03b4c961276b8cc7
eduardo-toste <duciochetti@gmail.com>
2026-06-30 21:50:20 -0300

    feat: implement login service method

--- src/main/java/com/auth/lab/service/AuthService.java
+++ src/main/java/com/auth/lab/service/AuthService.java
@@ -1,5 +1,8 @@
 package com.auth.lab.service;
 
+import com.auth.lab.configuration.TokenProvider;
+import com.auth.lab.dto.LoginRequest;
+import com.auth.lab.dto.LoginResponse;
 import com.auth.lab.dto.RegisterRequest;
 import com.auth.lab.exception.RoleNotFoundException;
 import com.auth.lab.exception.UserAlreadyExistsException;
@@ -7,7 +10,14 @@ import com.auth.lab.model.Role;
 import com.auth.lab.model.User;
 import com.auth.lab.repository.RoleRepository;
 import com.auth.lab.repository.UserRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.BadRequestException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +30,11 @@ public class AuthService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final TokenProvider tokenProvider;
+
+    @Value("${jwt.expiration.time}")
+    private long expirationTime;
 
     public void registerUser(RegisterRequest request) {
         checkIfUserAlreadyExists(request.email());
@@ -33,6 +48,18 @@ public class AuthService {
                 .build();
 
         userRepository.save(user);
+    }
+
+    public LoginResponse login(LoginRequest request) throws Exception {
+        try {
+            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.email(), request.password()));
+            String token = tokenProvider.generateToken(authentication);
+            return new LoginResponse(token, expirationTime);
+        } catch (BadCredentialsException e) {
+            throw new BadRequestException("Invalid credentials.");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private Role loadDefaultRole() {
