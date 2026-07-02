b1df5c41b2f80796821d776779e592dbda662adc
eduardo-toste <duciochetti@gmail.com>
2026-07-01 21:36:14 -0300

    feat: implement find methods

--- src/main/java/com/auth/lab/repository/NoteRepository.java
+++ src/main/java/com/auth/lab/repository/NoteRepository.java
@@ -6,10 +6,17 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
 
     @EntityGraph(attributePaths = "author")
     List<Note> findAllByAuthor(User author);
+
+    @EntityGraph(attributePaths = "author")
+    Optional<Note> findByIdAndAuthor(Long noteId, User requester);
+
+    @EntityGraph(attributePaths = "author")
+    Optional<Note> findById(Long noteId);
 
 }
